ff80a5887994caacc365a4c5d4715be9964845f9
SoftDevCLF <cintya.lara.f@gmail.com>
2025-09-18 17:37:27 -0600

    ✏️ Modify some styles in Link

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -2,13 +2,14 @@ import Link from "next/link";
 
 export default function StudentInfo() {
   return (
-    <section className="m-3">
-      <h2>Name:Cintya Lara</h2>
+    <section>
+      <h2>Name: Cintya Lara</h2>
+      Github:
       <Link
-        className="underline text-blue-500 hover:text-blue-800"
+        className="underline text-blue-500 hover:text-blue-800 ml-1"
         href="https://github.com/SoftDevCLF/cprg306-assignments"
       >
-        github: https://github.com/SoftDevCLF/cprg306-assignments
+        https://github.com/SoftDevCLF/cprg306-assignments
       </Link>
     </section>
   );
