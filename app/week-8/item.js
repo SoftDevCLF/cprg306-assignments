cfeaefa9a781319466f85d13cfa4b6c23ea455c9
SoftDevCLF <cintya.lara.f@gmail.com>
2025-10-31 18:35:47 -0600

    ➕ Add files from week 7

---
+++ app/week-8/item.js
@@ -0,0 +1,23 @@
+export default function Item({ name, quantity, category }) {
+  return (
+    <ItemCard>
+      <ul>
+        <li>{name}</li>
+        <li>Quantity: {quantity}</li>
+        <li className="capitalize">Category: {category}</li>
+      </ul>
+    </ItemCard>
+  );
+}
+
+function ItemCard({ children }) {
+  return (
+    <div className="bg-white w-72 mx-auto border-gray-300 border-2 p-2 rounded-lg shadow-md flex justify-start mb-2">
+      <div className="pr-4 border-blue-400 border-r-2">
+        <input type="checkbox" />
+      </div>
+
+      <div className="w-full pl-2">{children}</div>
+    </div>
+  );
+}
