3411727feb6ec0afb845f8295be57773fe3594dc
SoftDevCLF <cintya.lara.f@gmail.com>
2025-10-31 22:24:52 -0600

    🔨 Add handleItemSelect function and MealIdeas component. Minor fixes on layout.

--- app/week-8/page.js
+++ app/week-8/page.js
@@ -3,22 +3,59 @@ import { useState } from "react";
 import NewItem from "./new-item";
 import ItemList from "./item-list";
 import itemsData from "./items.json";
+import MealIdeas from "./meal-ideas";
 
 export default function Page() {
   const [items, setItems] = useState(itemsData);
+  const [selectedItemName, setSelectedItemName] = useState("");
   function handleAddItem(newItem) {
     setItems([...items, newItem]);
   }
 
+  function handleItemSelect(itemName) {
+    if (!itemName) {
+      return;
+    }
+    let cleanedItemName = itemName.trim().toLowerCase();
+    cleanedItemName = cleanedItemName
+      .split(",")[0]
+      .replace(
+        /[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2580-\u27BF]|\uD83E[\uDD10-\uDDFF]| /g,
+        ""
+      );
+    console.log("Selected item:", cleanedItemName);
+    setSelectedItemName(cleanedItemName);
+  }
+
   return (
-    <main className="min-h-screen bg-gray-50 p-6">
-      <h1 className="font-bold text-2xl text-center mb-6">Shopping List</h1>
-      <section>
-        <NewItem onAddItem={handleAddItem} />
-      </section>
-      <section className="mt-6">
-        <ItemList items={items} />
-      </section>
+    <main className="bg-gray-5 p-4 max-w-5xl mx-auto">
+      <h1 className="font-bold text-2xl text-center mb-6">
+        Shopping List + Meal Ideas
+      </h1>
+      <div className="grid md:grid-cols-2 gap-6">
+        <div>
+          <NewItem onAddItem={handleAddItem} />
+
+          <ItemList items={items} onItemSelect={handleItemSelect} />
+        </div>
+        <div>
+          <h2 className="font-bold mb-2">
+            {selectedItemName
+              ? `Meal Ideas for "${selectedItemName}"`
+              : `Meal Ideas (select an item)`}
+          </h2>
+
+          <div>
+            {selectedItemName ? (
+              <MealIdeas ingredient={selectedItemName} />
+            ) : (
+              <p className="text-md text-gray-500">
+                Choose an item to see ideas.
+              </p>
+            )}
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
