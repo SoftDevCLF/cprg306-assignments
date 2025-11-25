7225b7f9b51d0a3ad3d5bce603f2daad41721467
SoftDevCLF <cintya.lara.f@gmail.com>
2025-11-24 23:44:58 -0700

    🛠️Add style

--- app/page.js
+++ app/page.js
@@ -10,31 +10,31 @@ export default function Home() {
       </header>
       <section>
         <ul>
-          <li className="text-blue-400 hover:text-blue-700 mt-3 hover:underline rounded-lg border border-gray-300 p-2 max-w-xs mx-auto px-10 py-4 text-center">
+          <li className="text-blue-400 hover:text-blue-700 mt-3 hover:underline rounded-lg border border-gray-300 p-2 max-w-xs mx-auto px-10 py-4 text-center dark:bg-gray-800 dark:border-gray-800 shadow-md">
             <Link href="/week-2">Go to Week 2</Link>
           </li>
-          <li className="text-blue-400 hover:text-blue-700 mt-3 hover:underline rounded-lg border border-gray-300 p-2 max-w-xs mx-auto px-10 py-4 text-center">
+          <li className="text-blue-400 hover:text-blue-700 mt-3 hover:underline rounded-lg border border-gray-300 p-2 max-w-xs mx-auto px-10 py-4 text-center dark:bg-gray-800 dark:border-gray-800 shadow-md">
             <Link href="/week-3">Go to Week 3</Link>
           </li>
-          <li className="text-blue-400 hover:text-blue-700 mt-3 hover:underline rounded-lg border border-gray-300 p-2 max-w-xs mx-auto px-10 py-4 text-center">
+          <li className="text-blue-400 hover:text-blue-700 mt-3 hover:underline rounded-lg border border-gray-300 p-2 max-w-xs mx-auto px-10 py-4 text-center dark:bg-gray-800 dark:border-gray-800 shadow-md">
             <Link href="/week-4">Go to Week 4</Link>
           </li>
-          <li className="text-blue-400 hover:text-blue-700 mt-3 hover:underline rounded-lg border border-gray-300 p-2 max-w-xs mx-auto px-10 py-4 text-center">
+          <li className="text-blue-400 hover:text-blue-700 mt-3 hover:underline rounded-lg border border-gray-300 p-2 max-w-xs mx-auto px-10 py-4 text-center dark:bg-gray-800 dark:border-gray-800 shadow-md">
             <Link href="/week-5">Go to Week 5</Link>
           </li>
-          <li className="text-blue-400 hover:text-blue-700 mt-3 hover:underline rounded-lg border border-gray-300 p-2 max-w-xs mx-auto px-10 py-4 text-center">
+          <li className="text-blue-400 hover:text-blue-700 mt-3 hover:underline rounded-lg border border-gray-300 p-2 max-w-xs mx-auto px-10 py-4 text-center dark:bg-gray-800 dark:border-gray-800 shadow-md">
             <Link href="/week-6">Go to Week 6</Link>
           </li>
-          <li className="text-blue-400 hover:text-blue-700 mt-3 hover:underline rounded-lg border border-gray-300 p-2 max-w-xs mx-auto px-10 py-4 text-center">
+          <li className="text-blue-400 hover:text-blue-700 mt-3 hover:underline rounded-lg border border-gray-300 p-2 max-w-xs mx-auto px-10 py-4 text-center dark:bg-gray-800 dark:border-gray-800 shadow-md">
             <Link href="/week-7">Go to Week 7</Link>
           </li>
-          <li className="text-blue-400 hover:text-blue-700 mt-3 hover:underline rounded-lg border border-gray-300 p-2 max-w-xs mx-auto px-10 py-4 text-center">
+          <li className="text-blue-400 hover:text-blue-700 mt-3 hover:underline rounded-lg border border-gray-300 p-2 max-w-xs mx-auto px-10 py-4 text-center dark:bg-gray-800 dark:border-gray-800 shadow-md">
             <Link href="/week-8">Go to Week 8</Link>
           </li>
-          <li className="text-blue-400 hover:text-blue-700 mt-3 hover:underline rounded-lg border border-gray-300 p-2 max-w-xs mx-auto px-10 py-4 text-center">
+          <li className="text-blue-400 hover:text-blue-700 mt-3 hover:underline rounded-lg border border-gray-300 p-2 max-w-xs mx-auto px-10 py-4 text-center dark:bg-gray-800 dark:border-gray-800 shadow-md">
             <Link href="/week-9">Go to Week 9</Link>
           </li>
-          <li className="text-blue-400 hover:text-blue-700 mt-3 hover:underline rounded-lg border border-gray-300 p-2 max-w-xs mx-auto px-10 py-4 text-center">
+          <li className="text-blue-400 hover:text-blue-700 mt-3 hover:underline rounded-lg border border-gray-300 p-2 max-w-xs mx-auto px-10 py-4 text-center dark:bg-gray-800 dark:border-gray-800 shadow-md">
             <Link href="/week-10">Go to Week 10</Link>
           </li>
         </ul>
