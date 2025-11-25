7c08011d6c3df578dc5c3b1485a7700337b38e87
SoftDevCLF <cintya.lara.f@gmail.com>
2025-11-24 23:47:59 -0700

    📝Correct width

--- app/week-10/shopping-list/item.js
+++ app/week-10/shopping-list/item.js
@@ -2,7 +2,7 @@ export default function Item({ name, quantity, category, onSelect }) {
   return (
     <div
       onClick={onSelect}
-      className="cursor-pointer bg-white w-72 mx-auto border-gray-300 border-2 p-2 rounded-lg shadow-md flex justify-start mb-2 hover:bg-blue-50 transition dark:bg-gray-800 dark:border-gray-800 dark:hover:bg-gray-700"
+      className="cursor-pointer bg-white w-full mx-auto border-gray-300 border-2 p-2 rounded-lg shadow-md flex justify-start mb-2 hover:bg-blue-50 transition dark:bg-gray-800 dark:border-gray-800 dark:hover:bg-gray-700"
     >
       <div className="w-full pl-2">
         <ul>
